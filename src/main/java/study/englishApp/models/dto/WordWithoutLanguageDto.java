f7a202cf956d27f19e72eb2fb650e5a1acffe25a
Ilya Knor <ilyaknor@gmail.com>
2021-08-18 13:44:04 +0300

    Тест

--- src/main/java/study/englishApp/models/dto/WordWithoutLanguageDto.java
+++ src/main/java/study/englishApp/models/dto/WordWithoutLanguageDto.java
@@ -2,11 +2,13 @@ package study.englishApp.models.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class WordWithoutLanguageDto {
     private Long id;
     private String words;
--- src/main/java/study/englishApp/service/WordServiceImpl.java
+++ src/main/java/study/englishApp/service/WordServiceImpl.java
@@ -1,9 +1,6 @@
 package study.englishApp.service;
 
 import org.springframework.stereotype.Service;
-import study.englishApp.convertors.WordCreationDtoConverter;
-import study.englishApp.convertors.WordUpdatingDtoConverter;
-import study.englishApp.convertors.WordWithoutLanguageDtoConverter;
 import study.englishApp.models.Language;
 import study.englishApp.models.Word;
 import study.englishApp.models.dto.WordCreationDto;
@@ -19,28 +16,19 @@ public class WordServiceImpl implements WordService{
 
     private final WordRepository wordRepository;
     private final LanguageService languageService;
-    private final WordCreationDtoConverter wordCreationDtoConverter;
-    private final WordUpdatingDtoConverter wordUpdatingDtoConverter;
-    private final WordWithoutLanguageDtoConverter wordWithoutLanguageDtoConverter;
 
-    public WordServiceImpl(WordRepository wordRepository, LanguageService languageService,
-                           WordCreationDtoConverter wordCreationDtoConverter, WordUpdatingDtoConverter wordUpdatingDtoConverter,
-                           WordWithoutLanguageDtoConverter wordWithoutLanguageDtoConverter) {
+    public WordServiceImpl(WordRepository wordRepository, LanguageService languageService) {
         this.wordRepository = wordRepository;
         this.languageService = languageService;
-        this.wordCreationDtoConverter = wordCreationDtoConverter;
-        this.wordUpdatingDtoConverter = wordUpdatingDtoConverter;
-        this.wordWithoutLanguageDtoConverter = wordWithoutLanguageDtoConverter;
     }
+
 
     @Override
     public Word create(WordCreationDto word) {
-        if (!wordRepository.existsByWordAndLang_Id(word.getWord(), word.getLangId())){
-//            Language language = languageService.read(word.getLangId());
-//            Word created = new Word(word.getWord(), language);
-
-
-            return wordRepository.save(wordCreationDtoConverter.convertToEntity(word));
+        if (!wordRepository.existsByWordAndLang_Id(word.getWord(), word.getLangId())) {
+            Language language = languageService.read(word.getLangId());
+            Word created = new Word(word.getWord(), language);
+            return wordRepository.save(created);
         }
         throw new RuntimeException("Слово на таком языке уже существует.");
     }
