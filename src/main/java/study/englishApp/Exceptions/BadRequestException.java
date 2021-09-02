cd5aecf55d89e758ba4b005caf42f1332debb229
Ilya Knor <ilyaknor@gmail.com>
2021-09-02 14:53:35 +0300

    Обработка исключений

---
+++ src/main/java/study/englishApp/Exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package study.englishApp.Exceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
