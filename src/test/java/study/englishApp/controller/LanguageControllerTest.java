97d19bc829886de076358206afdc77987b9be4b8
Ilya Knor <ilyaknor@gmail.com>
2021-09-20 12:31:39 +0300

    Тестирование контроллера

--- src/test/java/study/englishApp/controller/LanguageControllerTest.java
+++ src/test/java/study/englishApp/controller/LanguageControllerTest.java
@@ -1,34 +1,65 @@
 package study.englishApp.controller;
 
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import study.englishApp.models.dto.LanguageDto;
+import study.englishApp.service.LanguageService;
 
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class LanguageControllerTest {
-    @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    @Mock
+    private LanguageService languageService;
+
+    @InjectMocks
+    private LanguageController languageController;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(languageController)
+                .build();
+    }
 
     @Test
-    public void createExistingLanguage() throws Exception {
-        LanguageDto dto = new LanguageDto("Ru");
+    public void shouldReturnNewLanguage() throws Exception {
+        LanguageDto dto = LanguageDto.builder()
+                .language("EN")
+                .build();
+
+        LanguageDto created = LanguageDto.builder()
+                .id(1L)
+                .language("EN")
+                .build();
+
+            when(languageService.create(any())).thenReturn(created);
+
+        ObjectMapper objectMapper = new ObjectMapper();
 
         mockMvc.perform(post("/languages")
-                .contentType("application/json")
-                .content(objectMapper.writeValueAsString(dto)))
-                .andExpect(status().isConflict());
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(objectMapper.writeValueAsString(created)));
+
     }
 }
